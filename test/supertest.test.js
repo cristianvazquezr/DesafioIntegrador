22cc87d8d4374cf56b65b79fbaac3478b52c52ba
cristianvazquezr <vazquezcristianr@gmail.com>
2023-11-30 16:14:06 -0300

    se agrega testing

--- test/supertest.test.js
+++ test/supertest.test.js
@@ -11,6 +11,11 @@ const expect = chai.expect
 const requester=supertest('http://localhost:8080')
 
 describe('corriendo test wepApp', ()=>{
+    beforeEach(async function(){
+        mongoose.connection.collections.products.drop()
+        mongoose.connection.collections.carts.drop()
+    })
+
     describe('testing products API',async()=>{
 
        let  product={
@@ -27,6 +32,7 @@ describe('corriendo test wepApp', ()=>{
             const {statusCode,ok,_body}= await requester.post('/api/products/').send(product)
             expect(statusCode).is.eqls(200)
             expect(_body.status).is.eqls('OK')
+
         }).timeout(10000)
 
         it('crear productos: api post debe arrojar error si se repite el code', async()=>{
@@ -35,12 +41,15 @@ describe('corriendo test wepApp', ()=>{
             expect(statusCode).is.eqls(400)
             expect(_body.status).is.eqls('error')
             expect(_body.message).is.eqls("ya existe producto con ese code")
+
         }).timeout(10000)
 
         it('Get producto: api get debe obtener todos los productos', async()=>{
+
             const {statusCode,ok,_body}= await requester.get(`/api/products/`)
             expect(statusCode).is.eqls(200)
             expect(_body.payLoad[0]).is.ok.and.to.have.property("_id")
+
         }).timeout(10000)
        
     })
@@ -56,25 +65,28 @@ describe('corriendo test wepApp', ()=>{
  
         it('agregar producto al carrito: api post debe tirar error si el producto es de su autoria', async()=>{
 
+            //obtengo los carritos
             const getCarts=await cartDao.getCarts()
+            //busco un ID
             const idCarts=getCarts[0]._id
 
+            // creo un producto y busco su ID
             const addProduct= await productDao.addProduct("title", "description", "category", 100, "thumbnail", 1, 1423, 'premium');
             const getProducts=await productDao.getProducts({})
             const idProduct=getProducts.payLoad[0]._id
 
-
-            
+            // agrego el producto al carrito
             const {statusCode,ok,_body}= await requester.post(`/api/carts/${idCarts}/product/${idProduct}`).send({quantity:10})
             expect(statusCode).is.eqls(500)
             expect(_body.status).is.eqls('error')
             expect(_body.message).is.eqls("no puede agregar al carrito su propio producto")
         }).timeout(10000)
  
-         it('Get carts: api get debe obtener todos los carts', async()=>{
-             const {statusCode,ok,_body}= await requester.get(`/api/carts/`)
-             expect(statusCode).is.eqls(200)
-         }).timeout(10000)
+        it('Get carts: api get debe obtener todos los carts', async()=>{
+            const {statusCode,ok,_body}= await requester.get(`/api/carts/`)
+            expect(statusCode).is.eqls(200)
+        }).timeout(10000)
         
-     })
-})
+    })
+
+}).timeout(70000)
